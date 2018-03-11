cba39d94bb1e8a369d9d6ad3806283ebcf0a13d2
Ivan Greene <ivan@ivan.sh>
2018-03-11 17:34:01 -0500

    1.042

---
+++ 1/1.042.scm
@@ -0,0 +1,11 @@
+;; Q: Let f and g be two one-argument functions. The composition f after g is defined to be the function x -> f(g(x)). Define a procedure compose that implements composition. For example, if inc is a procedure that adds 1 to its argument,
+
+;; ((compose square inc) 6)
+
+;; 49
+
+;; A:
+
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
