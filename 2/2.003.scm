722e57ff26a09ad4830df29c7838685f5e111433
Ivan Greene <ivan@ivan.sh>
2018-03-13 18:41:58 -0500

    2.003 done

--- 2/2.003.scm
+++ 2/2.003.scm
@@ -54,5 +54,43 @@
 ;; store to represent a rectangle. Truly all we need to construct it is two
 ;; points. Would it make sense to represent this using 4 segments? I think not.
 ;; Then again, if we consider rectangles that do not only lie squarely on a
-;; plane, we would need more information. I went for a simplified approach that
-;; represents the rectangle using four points, constructed from two points.
+;; plane, we would need more information. I at first went for what I thought
+;; was a simplified approach that represents the rectangle using four points,
+;; constructed from two points.
+
+(define (make-rectangle p1 p2 p3 p4)
+  (cons (cons p1 p2)
+        (cons p3 p4)))
+
+;; Changing the constructor to be able to handle more complicated rectangles
+;; not only lying squarely on the plane was trivial! In fact, it simplified the
+;; constructor procedure (although it now requires more data from the user of
+;; the constructor.) This constructor also assumes that the points are ordered.
+
+;; To (inefficiently and redundantly) represent a rectangle as four segments,
+;; our area/perimeter procedures need not be changed, only our constructor and
+;; selectors:
+
+(define (make-rectangle p1 p2 p3 p4)
+  (cons (cons (make-segment p1 p2)
+              (make-segment p2 p3))
+        (cons (make-segment p3 p4)
+              (make-segment p4 p1))))
+
+(define (rect-side-a rect)
+  (car (car rect)))
+
+(define (rect-side-b rect)
+  (cdr (car rect)))
+
+(define (rect-point-1 rect)
+  (start-segment (car (car rect))))
+
+(define (rect-point-2 rect)
+  (start-segment (cdr (car rect))))
+
+(define (rect-point-3 rect)
+  (start-segment (car (cdr rect))))
+
+(define (rect-point-4 rect)
+  (start-segment (cdr (cdr rect))))
