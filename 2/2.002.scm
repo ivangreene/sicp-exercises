80a1b34f92a2834a6a1606048ef5b2844776e88b
Ivan Greene <ivan@ivan.sh>
2018-03-15 15:34:59 -0500

    2.002 done

--- 2/2.002.scm
+++ 2/2.002.scm
@@ -1,4 +1,15 @@
-;; Q: Consider the problem of representing line segments in a plane. Each segment is represented as a pair of points: a starting point and an ending point. Define a constructor make-segment and selectors start-segment and end-segment that define the representation of segments in terms of points. Furthermore, a point can be represented as a pair of numbers: the x coordinate and the y coordinate. Accordingly, specify a constructor make-point and selectors x-point and y-point that define this representation. Finally, using your selectors and constructors, define a procedure midpoint-segment that takes a line segment as argument and returns its midpoint (the point whose coordinates are the average of the coordinates of the endpoints). To try your procedures, you'll need a way to print points:
+;; Q: Consider the problem of representing line segments in a plane. Each
+;; segment is represented as a pair of points: a starting point and an ending
+;; point. Define a constructor make-segment and selectors start-segment and
+;; end-segment that define the representation of segments in terms of points.
+;; Furthermore, a point can be represented as a pair of numbers: the x
+;; coordinate and the y coordinate. Accordingly, specify a constructor
+;; make-point and selectors x-point and y-point that define this
+;; representation. Finally, using your selectors and constructors, define a
+;; procedure midpoint-segment that takes a line segment as argument and returns
+;; its midpoint (the point whose coordinates are the average of the coordinates
+;; of the endpoints). To try your procedures, you'll need a way to print
+;; points:
 
 (define (print-point p)
   (display "(")
@@ -10,9 +21,8 @@
 
 ;; A:
 
-(define (make-segment start-x start-y end-x end-y)
-  (cons (make-point start-x start-y)
-        (make-point end-x end-y)))
+(define (make-segment p1 p2)
+  (cons p1 p2))
 
 (define (start-segment seg)
   (car seg))
@@ -46,3 +56,11 @@
                     (x-point (end-segment seg))) 2)
               (/ (+ (y-point (start-segment seg))
                     (y-point (end-segment seg))) 2)))
+
+(define (square x) (* x x))
+
+(define (segment-length seg)
+  (sqrt (+ (square (- (x-point (end-segment seg))
+                      (x-point (start-segment seg))))
+           (square (- (y-point (end-segment seg))
+                      (y-point (start-segment seg)))))))
