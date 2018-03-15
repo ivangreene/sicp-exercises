b808e6e8578fe0bb82591ad057e58d927a2a883a
Ivan Greene <ivan@ivan.sh>
2018-03-15 17:00:15 -0500

    2.011... nope...

---
+++ 2/2.011.scm
@@ -0,0 +1,7 @@
+;; Q: In passing, Ben also cryptically comments: "By testing the signs of the
+;; endpoints of the intervals, it is possible to break mul-interval into nine
+;; cases, only one of which requires more than two multiplications." Rewrite
+;; this procedure using Ben's suggestion.
+
+;; R (for retort): Ben seriously needs to fuck off and this suggestion has no
+;; place in a mature computer programmer's thought process :~)
