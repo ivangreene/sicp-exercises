0c393f5809b0ff3d64364492765bd2892ac17487
Ivan Greene <ivan@ivan.sh>
2018-03-16 16:41:04 -0500

    intervals.scm: add constructor/selectors

--- 2/intervals.scm
+++ 2/intervals.scm
@@ -1,3 +1,7 @@
+(define (make-interval a b) (cons a b))
+(define (lower-bound i) (car i))
+(define (upper-bound i) (cdr i))
+
 (define (add-interval x y)
   (make-interval (+ (lower-bound x) (lower-bound y))
                  (+ (upper-bound x) (upper-bound y))))
