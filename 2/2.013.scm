53f3221e56a156a11c38caaf7d14760f69d1088a
Ivan Greene <ivan@ivan.sh>
2018-03-16 16:41:33 -0500

    2.013: done

---
+++ 2/2.013.scm
@@ -0,0 +1,7 @@
+;; Q: Show that under the assumption of small percentage tolerances there is a
+;; simple formula for the approximate percentage tolerance of the product of
+;; two intervals in terms of the tolerances of the factors. You may simplify
+;; the problem by assuming that all numbers are positive.
+
+;; A: The percentage tolerance of a product of two intervals will be
+;; approximately the sum of the percentage tolerances of the factors.
