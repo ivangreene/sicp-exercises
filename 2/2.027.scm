2fe38176217f4cde0dd6d5a001ea4f452e603562
Ivan Greene <ivan@ivan.sh>
2018-05-13 16:45:44 -0500

    2.027 done

---
+++ 2/2.027.scm
@@ -0,0 +1,30 @@
+;; Q: Modify your reverse procedure of exercise 2.18 to produce a deep-reverse
+;; procedure that takes a list as argument and returns as its value the list
+;; with its elements reversed and with all sublists deep-reversed as well. For
+;; example,
+
+(define x (list (list 1 2) (list 3 4)))
+
+x
+; ((1 2) (3 4))
+
+(reverse x)
+; ((3 4) (1 2))
+
+; (deep-reverse x)
+; ((4 3) (2 1))
+
+;; A:
+
+(define (deep-reverse lis)
+  (define (reverse-iter lis result)
+    (if (null? lis)
+        result
+        (reverse-iter
+          (cdr lis)
+          (cons
+          (if (and (list? (car lis)) (not (null? (car lis))))
+              (deep-reverse (car lis))
+              (car lis))
+          result))))
+  (reverse-iter lis nil))
