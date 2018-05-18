6a83b0c524b6d280d9f948248f1225ed23937b6f
Ivan Greene <ivan@ivan.sh>
2018-05-18 17:57:19 -0500

    2.042 done

--- 2/2.042.scm
+++ 2/2.042.scm
@@ -45,4 +45,67 @@
 
 ;; A:
 
-;; Initially, I think the most natural solution would be a list of numbers representing columns, with each number corresponding to the row that a queen is in for that column.
+;; Initially, I think the most natural solution would be a list of numbers
+;; representing columns, with each number corresponding to the row that a queen
+;; is in for that column.
+
+; not-in? as a predicate will help us define no-adjacents?
+
+(define (not-in? item seq)
+  (cond ((null? seq) #t)
+        ((= (car seq) item) #f)
+        (else (not-in? item (cdr seq)))))
+
+; empty-board will be an empty list
+
+(define empty-board nil)
+
+; adjoin-position disregards k and prepends the new-row to the rest
+
+(define (adjoin-position new-row k rest)
+  (cons new-row rest))
+
+; no-adjacents simply calls not-in? to make sure that no other queens are
+; placed in the same row
+
+(define (no-adjacents? positions)
+  (not-in? (car positions) (cdr positions)))
+
+; no-diagonals? iterates through to the end of our board configuration list to
+; determine if any pieces in the rest of the board conflict diagonally with the
+; latest piece, comparing the row of each piece with the row of our current
+; piece, plus or minus the offset. This means, for example, if our current
+; piece was in row 3, the piece in the next column may not be in rows 3+1 or
+; 3-1, the piece in the column after that may not be in rows 3+2 or 3-2, etc.
+
+(define (no-diagonals? positions)
+  (define (iter row offset rest)
+    (cond ((null? rest) #t)
+          ((or (= (car rest) (- row offset))
+               (= (car rest) (+ row offset)))
+           #f)
+          (else (iter row (+ offset 1) (cdr rest)))))
+  (iter (car positions) 1 (cdr positions)))
+
+; combine these two predicates to create safe?
+
+(define (safe? k positions)
+  (and (no-adjacents? positions)
+       (no-diagonals? positions)))
+
+; for an imporved version, we could just combine the two operations since we're
+; iterating through the list already:
+
+(define (safe? k positions)
+  (define (iter row offset rest)
+    (cond ((null? rest) #t)
+          ((or (= (car rest) (- row offset))
+               (= (car rest) row)
+               (= (car rest) (+ row offset)))
+           #f)
+          (else (iter row (+ offset 1) (cdr rest)))))
+  (iter (car positions) 1 (cdr positions)))
+
+;; Throughout this exercise, I didn't need to make use of k at all. I'm curious
+;; as to whether the authors included k as a parameter to confuse, or if their
+;; intended solution relied upon it.
